0bdff00792503d09fa66964f5d7cfaaeb648eac4
CIRCULARKA <donkardon@list.ru>
2020-11-24 15:08:10 +0500

    Fix DeviceHistoryWindow

--- src/AuthDemo/SuperUserWindow/Dialogs/DeviceHistoryWindow/DeviceHistoryWindow.xaml.cs
+++ src/AuthDemo/SuperUserWindow/Dialogs/DeviceHistoryWindow/DeviceHistoryWindow.xaml.cs
@@ -1,12 +1,18 @@
+using System.Windows.Input;
+
 namespace AuthDemo
 {
-	internal class DeviceHistoryWindow : MonoWindowBehavior
+	public partial class DeviceHistoryWindow : MonoWindowBehavior
 	{
 		public DeviceHistoryWindow(SuperUserWindow parentWindow)
 		{
 			ParentWindow = parentWindow;
+
+			InitializeComponent();
 		}
 
 		private SuperUserWindow ParentWindow { get; set; }
+
+		private void OnHistoryGridScroll(object sender, MouseWheelEventArgs info) { }
 	}
 }
