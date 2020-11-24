e583bfc4f6a813ad725bf72e9410435b4d0499f3
CIRCULARKA <donkardon@list.ru>
2020-11-24 15:35:32 +0500

    Add FillHistoryGridWithItems method

--- src/AuthDemo/SuperUserWindow/Dialogs/DeviceHistoryWindow/DeviceHistoryWindow.xaml.cs
+++ src/AuthDemo/SuperUserWindow/Dialogs/DeviceHistoryWindow/DeviceHistoryWindow.xaml.cs
@@ -1,17 +1,35 @@
 using System.Windows.Input;
+using System.Collections.Generic;
+
+using AuthDemo.Data;
 
 namespace AuthDemo
 {
 	public partial class DeviceHistoryWindow : MonoWindowBehavior
 	{
+		private readonly List<History> _historyNotes;
+
 		public DeviceHistoryWindow(SuperUserWindow parentWindow)
 		{
 			ParentWindow = parentWindow;
 
+			_historyNotes = new List<History>();
+
 			InitializeComponent();
+			FillHistoryGridWithItems();
 		}
 
 		private SuperUserWindow ParentWindow { get; set; }
+
+		private void FillHistoryGridWithItems()
+		{
+			_historyNotes.AddRange(
+				History.GetDeviceHistory(
+					ParentWindow.SelectedDevice
+				)
+			);
+			historyGrid.ItemsSource = _historyNotes;
+		}
 
 		private void OnHistoryGridScroll(object sender, MouseWheelEventArgs info) { }
 	}
